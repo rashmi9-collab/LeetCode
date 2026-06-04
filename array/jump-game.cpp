23f04529a2d9766d6ff841fa40445abf1bf13404
Rashmi Goswami <stl.2322rashmi9@gmail.com>
2026-06-04 19:22:36 +0530

    LC-jump-game [Runtime: N/A; Memory: N/A]

--- array/jump-game.cpp
+++ array/jump-game.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
         int tmp = 1;
-for(int i=n-1; i>=0;i--)
-{
-if( nums[i] >= tmp) tmp=1;
-else tmp++;
-}
-if(tmp==1) 
- return true;
 
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] >= tmp)
+                tmp = 1;
+            else
+                tmp++;
+        }
+
+        return tmp == 1;
     }
-    return false;
 };
