f7c4ecd9cd783b846c98bf9c7771dae9601de252
Rashmi Goswami <stl.2322rashmi9@gmail.com>
2026-06-04 19:20:59 +0530

    LC-jump-game [Runtime: N/A; Memory: N/A]

---
+++ greedy/jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int tmp = 1;
+for(int i=n-1; i>=0;i--)
+{
+if( nums[i] >= tmp) tmp=1;
+else tmp++;
+}
+if(tmp==1) 
+true;
+else 
+false;
+    }
+};
