7e9ab67249f7ec4c87e344908ab8b5194ce3562b
Rashmi Goswami <stl.2322rashmi9@gmail.com>
2026-07-01 19:16:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (97.22%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+
+    long long power(long long x, long long n) {
+        long long ans = 1;
+
+        while (n > 0) {
+
+            if (n % 2 == 1)
+                ans = (ans * x) % MOD;
+
+            x = (x * x) % MOD;
+            n /= 2;
+        }
+
+        return ans;
+    }
+
+    int countGoodNumbers(long long n) {
+
+        long long even = (n + 1) / 2;
+        long long odd = n / 2;
+
+        return (power(5, even) * power(4, odd)) % MOD;
+    }
+};
